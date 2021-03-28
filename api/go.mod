d10dc886b65a1906229248ce5a4b0d0a9776c5b3
Sam Lane <samuel.lane@hotmail.com>
2021-03-28 12:29:13 +0100

    very work in progress autherization middleware

--- api/go.mod
+++ api/go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/fatih/structs v1.1.0
 	github.com/google/go-github v17.0.0+incompatible // indirect
 	github.com/google/go-github/v34 v34.0.0
 	golang.org/x/oauth2 v0.0.0-20210323180902-22b0adad7558
--- api/pkg/auth/auth.go
+++ api/pkg/auth/auth.go
@@ -1,9 +1,12 @@
 package auth
 
 import (
+	"os"
+	"reflect"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/fatih/structs"
 )
 
 type JwtHandler struct {
@@ -34,4 +37,36 @@ func (j *JwtHandler) GenerateToken(c Claims) (string, error) {
 		return "", err
 	}
 	return signedToken, nil
+}
+
+//InvalidateToken is used to sign a user out by setting the exp time to 0
+func (j *JwtHandler) InvalidateToken(token string) string {
+
+	return ""
+}
+
+func (j *JwtHandler) TokenHasClaims(t *string, c *Claims) bool {
+	token, err := jwt.Parse(*t, func(t *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_SECRET_KEY")), nil
+	})
+	if err != nil {
+		return false
+	}
+
+	v := reflect.ValueOf(*c)
+	values := make([]string, v.NumField())
+
+	for i := 0; i < v.NumField(); i++ {
+		values[i] = v.Type().Field(i).Name
+	}
+
+	want := structs.Map(*c)
+	has := structs.Map(token.Claims)
+
+	for _, neededClaim := range values {
+		if want[neededClaim] != has[neededClaim] {
+			return false
+		}
+	}
+	return true
 }
