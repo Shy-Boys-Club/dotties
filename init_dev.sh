88c7d21f5935427c4b4ee32eed46821ca1dfb8af
Itzblend <huhta.lauri@gmail.com>
2021-03-29 17:08:51 +0300

    db on main docker compose file

--- init_dev.sh
+++ init_dev.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-docker-compose -f docker-compose-db.yml up -d
+#docker-compose -f docker-compose-db.yml up -d
+docker-compose up -d
 
 sleep 10
 
-docker cp local_db/  postgres-dotties:/
+docker cp db/local_db/  postgres-dotties:/
 
 docker exec -it -u postgres postgres-dotties psql -c "CREATE USER $LOCAL_DEV_DB_USER WITH SUPERUSER LOGIN PASSWORD $LOCAL_DEV_DB_PASSWD;"
 docker exec -it -u postgres postgres-dotties psql -f /local_db/scripts/create_db.sql
 
-cd local_db/scripts
+cd db/local_db/scripts
 python3 db.py
