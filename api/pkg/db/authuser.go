b21470a73eff3d54ab0e3d3bef253755868b7cea
Sam Lane <samuel.lane@hotmail.com>
2021-04-01 19:26:04 +0100

    added json fields

--- api/pkg/db/authuser.go
+++ api/pkg/db/authuser.go
@@ -6,11 +6,11 @@ import (
 
 type AuthUser struct {
 	ID                 uint64 `gorm:"primaryKey"`
-	GithubUsername     string
-	GithubAccessToken  string
-	GithubRefreshToken string
-	Email              string
-	LastActive         int64
+	GithubUsername     string `json:"github_username"`
+	GithubAccessToken  string `json:"github_access_token"`
+	GithubRefreshToken string `json:"github_refresh_token"`
+	Email              string `json:"email_address"`
+	LastActive         int64  `json:"last_active"`
 }
 
 // SetActive sets the users LastActive field to time.Now
