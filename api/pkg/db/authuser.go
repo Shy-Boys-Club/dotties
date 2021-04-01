4120909d5dec34bfb829e23b5ae34c0e69e14169
Sam Lane <samuel.lane@hotmail.com>
2021-04-01 19:21:15 +0100

    User struct updated to have last active field

--- api/pkg/db/authuser.go
+++ api/pkg/db/authuser.go
@@ -1,10 +1,19 @@
 package db
 
+import (
+	"time"
+)
+
 type AuthUser struct {
-    ID uint64 `gorm:"primaryKey"`
-    GithubUsername string
-    GithubAccessToken string
-    GithubRefreshToken string
-    Email string
+	ID                 uint64 `gorm:"primaryKey"`
+	GithubUsername     string
+	GithubAccessToken  string
+	GithubRefreshToken string
+	Email              string
+	LastActive         int64
 }
 
+// SetActive sets the users LastActive field to time.Now
+func (u *AuthUser) SetActive() {
+	u.LastActive = time.Now().Unix()
+}
